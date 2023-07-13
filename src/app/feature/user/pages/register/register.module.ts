38b4998dd2abcd36684d829fc489217629736fd8
kacper <106306274+kayYZ1@users.noreply.github.com>
2023-07-13 11:10:10 +0200

    feat: register form fields

--- src/app/feature/user/pages/register/register.module.ts
+++ src/app/feature/user/pages/register/register.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { RegisterComponent } from './register.component';
+import { ReturnArrowModule } from 'src/app/shared/components/return-arrow/return-arrow.module';
 
 @NgModule({
   declarations: [RegisterComponent],
   imports: [
-    CommonModule
+    CommonModule, ReturnArrowModule
   ]
 })
 export class RegisterModule { }
