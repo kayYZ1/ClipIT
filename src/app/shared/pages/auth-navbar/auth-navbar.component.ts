f86bdbc2469d02eb1a4715f67045ed1c05ccb7ef
kacper <106306274+kayYZ1@users.noreply.github.com>
2023-08-08 16:12:02 +0200

    userInfo obserable in ngOnInit

--- src/app/shared/pages/auth-navbar/auth-navbar.component.ts
+++ src/app/shared/pages/auth-navbar/auth-navbar.component.ts
@@ -1,24 +1,32 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 import { AuthService } from 'src/app/feature/user/services/AuthService';
 import { UserService } from 'src/app/feature/user/services/UserService';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-auth-navbar',
   templateUrl: './auth-navbar.component.html',
   styleUrls: ['./auth-navbar.component.scss'],
-  providers: [UserService, AuthService]
+  providers: [UserService, AuthService],
 })
 export class AuthNavbarComponent {
-  constructor(private _authService: AuthService, private _userService: UserService) {}
+  constructor(
+    private _authService: AuthService,
+    private _userService: UserService
+  ) {}
   @Output() logout: EventEmitter<void> = new EventEmitter<void>();
-  
-  userId = this._authService.userId
+  userId: string | null = null;
+  userInfo$: Observable<any> | null = null;
 
-  userInfo$ = this._userService.getSingleUser(this.userId as string)
-
+  ngOnInit() {
+    this.userId = this._authService.userId
+    if (this.userId) {
+      this.userInfo$ = this._userService.getSingleUser(this.userId as string)
+    }
+  }
 
   onLogout() {
     this.logout.emit();
-    console.log(this.userId)
+    console.log(this.userId);
   }
 }
