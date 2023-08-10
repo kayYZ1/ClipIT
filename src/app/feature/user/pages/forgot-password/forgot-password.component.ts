8abc3718fe6901d6ef97982af95bc126e944f4fe
kacper <106306274+kayYZ1@users.noreply.github.com>
2023-08-10 18:41:51 +0200

    password reset

--- src/app/feature/user/pages/forgot-password/forgot-password.component.ts
+++ src/app/feature/user/pages/forgot-password/forgot-password.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Validators, FormControl, FormGroup } from '@angular/forms';
+import { AuthService } from '../../services/AuthService';
 
 @Component({
   selector: 'app-forgot-password',
@@ -7,9 +8,19 @@ import { Validators, FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./forgot-password.component.scss'],
 })
 export class ForgotPasswordComponent {
+  constructor(private _authService: AuthService) {}
+
   resetPasswordForm = new FormGroup(
     {
       email: new FormControl("", [Validators.required, Validators.email])
     }
   )
+
+  onSubmit() {
+    try {
+      this._authService.userForgotPassword(this.resetPasswordForm.value.email as string)
+    } catch (error) {
+      console.log(error)
+    }
+  }
 }
